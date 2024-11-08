71f829933d886cfc5f9a26d19bb46dd00ee25b21
Davy <machadodavi3778@gmail.com>
2024-11-08 12:50:55 +0000

    Add files via upload

---
+++ Program546.cs
@@ -0,0 +1,17 @@
+﻿using System;
+public class D22
+{
+    //exeercicio2
+    public static void Main(string[] args)
+    {
+        int soma = 0;
+        for (int i = 1; i < 50; i++)
+        {
+            if (i % 2 != 0)
+            {
+                soma = soma + i;
+            }
+        }
+        Console.WriteLine("total é " + soma);
+    }
+}    
